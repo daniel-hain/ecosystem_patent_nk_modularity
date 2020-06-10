e914661e13ab55620e2512edd258af56cc01a80b
Daniel S. Hain <38432063+daniel-hain@users.noreply.github.com>
2020-06-10 16:31:39 +0200

    minor

--- 02_networks.r
+++ 02_networks.r
@@ -13,6 +13,22 @@ library(magrittr)
 pat_data <- readRDS("../input/pat_data.rds")
 
 ############################################################################
+# IPC classes
+############################################################################
+
+appln_ipc <- readRDS("../input/pat_appln_ipc.rds")
+
+# Patent country by first inventor
+pat_data %<>%
+  left_join(
+    appln_ipc %>%
+      filter(ipc_position == "F") %>%
+      select(appln_id, ipc_class_symbol), 
+    by = 'appln_id') %>%
+  rename(ipc = ipc_class_symbol) %>%
+  mutate(ipc4 = ipc %>% str_sub(1,4))
+
+############################################################################
 # Person
 ############################################################################
 
@@ -64,39 +80,38 @@ el <- el_docdb_cit_ipc %>%
 
 library(blockmodeling)
 library(Matrix)
-library(igraph)
 library(tidygraph)
 library(ggraph)
 
-g <- el %>% graph.data.frame(directed = TRUE) 
-g %<>% delete_vertices(strength(g, mode = "all") < 100)
-g %<>% simplify()
+g <- el %>% graph.data.frame(directed = TRUE) %>% 
+  simplify() %>%
+  as_tbl_graph(directed = TRUE)
 
-mat <- g %>% get.adjacency(sparse = FALSE, attr='weight')
+# Reduce grap
+g <- g %N>%
+  filter(centrality_degree(mode = 'all', normalized = TRUE, weights = weight) >= 10000)
 
-bm <- mat %>% optRandomParC(k = 4, 
-                            rep = 10, 
-                            mingr = 5,
-                            #isSym = FALSE,
-                            #diag = FALSE,
-                            approaches = "val", 
-                            blocks = c("com", "reg", "rfn", "cfn", "avg",  "nul")
+bm <- g %>% 
+  get.adjacency(sparse = FALSE, attr='weight') %>% 
+  optRandomParC(k = 4, 
+                rep = 10, 
+                mingr = 5,
+                #isSym = FALSE,
+                #diag = FALSE,
+                approaches = "val", 
+                blocks = c("com", "reg", "rfn", "cfn", "avg",  "nul")
                             )
 
 plot(bm)
 
-V(g)$opt.blocks <- bm$best$best1$clu
+# Add results
+g <- g %N>%
+  mutate(cluster_bm = bm$best$best1$clu)
 
-# Merge with patents
-pat_data
-
-# Plot
-plot.igraph(g, vertex.color=V(g)$opt.blocks) # plot in igraph
-
-# Plot hierarchical
-layout_h <- layout_with_sugiyama(g, layers = V(g)$opt.blocks, hgap=10, vgap=10, weights = E(g)$weight, attributes = 'all') 
-
-plot(layout_h$extd_graph, vertex.label.cex=0.5, vertex.size=9)
+# # Plot hierarchical
+# layout_h <- layout_with_sugiyama(g, layers = V(g)$cluster_bm, hgap=10, vgap=10, weights = E(g)$weight, attributes = 'all')
+# layout_h <- layout_h$extd_graph$layout
+# plot(layout_h$extd_graph, vertex.label.cex=0.5, vertex.size=9)
 
 layout_h$extd_graph %>%
   ggraph(layout = layout_h$extd_graph$layout) +
